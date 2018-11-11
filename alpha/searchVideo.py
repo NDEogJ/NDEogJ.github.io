a06a0ce2f0e608a74b5a9882172a72ec37bd18cb
NDEogJ <NDEogJ@users.noreply.github.com>
2018-11-11 04:13:05 -0600

    .

--- alpha/searchVideo.py
+++ alpha/searchVideo.py
@@ -2,10 +2,11 @@ from json import loads
 from browser import ajax
 from browser import document as doc
 from browser import window
+from url import link
 
 window.console.clear()
-
 key = "AIzaSyAqR6xo2RuEiYDjp10UI3dPtiw-q7scJsI"
+
 '''
 88888888ba      db         88888888ba    ad88888ba   88888888888
 88      "8b    d88b        88      "8b  d8"     "8b  88
@@ -29,15 +30,20 @@ except KeyError:
     page = None
     pageNum = None
 try:
-    vId = doc.query["id"]
+    vId = doc.query["vid"]
 except KeyError:
     vId = None
-print(f"VorC  -- {vorc}")
-print(f"Q     -- {q}")
-print(f"Order -- {order}")
-print(f"Video -- {vId}")
-print(f"Page  -- {page}")
-print(f"Page# -- {pageNum}")
+try:
+    cId = doc.query["cid"]
+except KeyError:
+    cId = None
+print(f"VorC   -- {vorc}")
+print(f"Q      -- {q}")
+print(f"Order  -- {order}")
+print(f"Video  -- {vId}")
+print(f"Channel-- {cId}")
+print(f"Page   -- {page}")
+print(f"Page#  -- {pageNum}")
 print('')
 
 
@@ -56,10 +62,12 @@ d8888b. db    db d8b   db
         GETc1()
     elif vId is not None:
         GETp1()
+    elif cId is not None:
+        GETu1()
     elif vorc is None:
         loaded(False)
-'''
-'''
+
+
 '''
 '''
 '''
@@ -88,8 +96,8 @@ Y8a     a8P  88            d8'        `8b   88     `8b    Y8a.    .a8P  88      
 '''
 '''
 '''
-'''
-'''
+
+
 def GETv1():
     '''
  d888b  d88888b d888888b      db    db       db
@@ -99,19 +107,7 @@ def GETv1():
 88. ~8~ 88.        88          `8bd8'        88
  Y888P  Y88888P    YP            YP          VP
     '''
-    url = f"https://www.googleapis.com/youtube/v3/search" \
-          f"?part=snippet" \
-          f"&maxResults=20" \
-          f"&order={order}" \
-          f"&q={q}" \
-          f"&relevanceLanguage=en" \
-          f"&type=video" \
-          f"&videoEmbeddable=true" \
-          f"&pageToken={page}" \
-          f"&videoSyndicated=true" \
-          f"&fields=items/id/videoId" \
-          f",nextPageToken,prevPageToken" \
-          f"&key={key}"
+    url = link(Url='v1', Order=order, Q=q, Page=page)
     req = ajax.ajax()
     req.bind('complete', DONEv1)
     req.open('GET', url, True)
@@ -131,9 +127,8 @@ Y8888D'  `Y88P'  VP   V8P Y88888P         YP          VP
     rawIDs = []
     if req.status == 200 or req.status == 0:
         data = loads(req.text)
-        global nextPAGE
+        global nextPAGE, prevPAGE
         nextPAGE = data.get("nextPageToken")
-        global prevPAGE
         prevPAGE = data.get("prevPageToken")
         if prevPAGE is None:
             prevPAGE = ' '
@@ -153,26 +148,8 @@ def GETv2(videoIDs):
 88. ~8~ 88.        88          `8bd8'       j88.
  Y888P  Y88888P    YP            YP         888888D
     '''
-    url = f"https://www.googleapis.com/youtube/v3/videos" \
-          f"?part=snippet," \
-          "statistics," \
-          "contentDetails" \
-          f"&id={videoIDs}" \
-          f"&fields=items" \
-          "(id" \
-          ",contentDetails" \
-          "/duration" \
-          ",snippet" \
-          "(channelTitle" \
-          ",thumbnails" \
-          "/medium" \
-          "/url" \
-          ",title)" \
-          ",statistics" \
-          "(viewCount))" \
-          f"&key={key}"
+    url = link(Url='v2', Id=videoIDs)
     req = ajax.ajax()
-    print(url)
     req.bind('complete', DONEv2)
     req.open('GET', url, True)
     req.set_header('content-type', 'application/x-www-form-urlencoded')
@@ -215,18 +192,16 @@ Y8888D'  `Y88P'  VP   V8P Y88888P         YP         888888D
             if vLEN == '0:0':
                 vLEN = "LIVE"
             print(f"{vID} -- VIDEO -- {vLEN.split()} -- {vTITLE}")
-            vRAWs.append(f"<li class='video'><a href='?id={vID}'>"
+            vRAWs.append(f"<li class='video'><a href='?vid={vID}'>"
                          f"<div class='img'><img src='{vIMG}' height='120px' width='210px'>"
                          f"<time> {vLEN} </time></div>"
                          f"<p class='title'>{vTITLE}</p></a>"
                          f"<p class='channel'>{cTITLE}"
                          f"<br>{vVIEWS} Views</p>"
                          f"</li>")
-        global vSTRs
+        global vSTRs, nextPAGEnum, prevPAGEnum
         vSTRs = "".join(vRAWs)
-        global nextPAGEnum
         nextPAGEnum = str(int(pageNum) + 1)
-        global prevPAGEnum
         if pageNum == "1":
             prevPAGEnum = "1"
         elif pageNum != "1":
@@ -269,8 +244,8 @@ db   8D 88   88 `8b  d8' `8b d8'8b d8'       `8bd8'
                        f"<button type='submit' name='page' value='{nextPAGE}'>&#8250;</button>" \
                        f"</form></li>"
     loaded(True)
-'''
-'''
+
+
 '''
 '''
 '''
@@ -299,8 +274,8 @@ Y8a     a8P  88            d8'        `8b   88     `8b    Y8a.    .a8P  88      
 '''
 '''
 '''
-'''
-'''
+
+
 def GETp1():
     '''
  d888b  d88888b d888888b      d8888b.       db
@@ -310,34 +285,13 @@ def GETp1():
 88. ~8~ 88.        88         88            88
  Y888P  Y88888P    YP         88            VP
     '''
-    url = f"https://www.googleapis.com/youtube/v3/videos" \
-          f"?part=snippet," \
-          "statistics," \
-          "contentDetails" \
-          f"&id={vId}" \
-          f"&fields=items" \
-          "(id" \
-          ",snippet" \
-          "(channelId" \
-          ",channelTitle" \
-          ",description" \
-          ",publishedAt" \
-          ",thumbnails" \
-          "/medium" \
-          "/url" \
-          ",title)" \
-          ",statistics" \
-          "(dislikeCount" \
-          ",likeCount" \
-          ",viewCount))" \
-          f"&key={key}"
+    url = link(Url='p1', Id=vId)
     req = ajax.ajax()
-    print(url)
     req.bind('complete', DONEp1)
     req.open('GET', url, True)
     req.set_header('content-type', 'application/x-www-form-urlencoded')
     req.send()
- 
+
 
 def DONEp1(req):
     '''
@@ -349,24 +303,34 @@ d8888b.  .d88b.  d8b   db d88888b      d8888b.       db
 Y8888D'  `Y88P'  VP   V8P Y88888P      88            VP
     '''
     data = loads(req.text)
-    print(data)
-    global link, title, views, channel, desc, likes, dislikes
-    link = f'https://www.youtube-nocookie.com/embed/' \
-           f'{data["items"][0]["id"]}' \
-           f'?rel=0'
-    title = data["items"][0]["snippet"]["title"]
-    channel = data["items"][0]["snippet"]["channelTitle"]
-    desc = data["items"][0]["snippet"]["description"]
+    global Vembed, Vtitle, Vviews, Vchannel, VchannelId, Vdesc, Vlikes, Vdislikes
+    Vembed = f'https://www.youtube-nocookie.com/embed/' \
+             f'{data["items"][0]["id"]}' \
+             f'?rel=0'
+    Vtitle = data["items"][0]["snippet"]["title"]
+    Vchannel = data["items"][0]["snippet"]["channelTitle"]
+    VchannelId = data["items"][0]["snippet"]["channelId"]
+    Vdesc = data["items"][0]["snippet"]["description"].replace('\n', ' <br> ')
+    lines = Vdesc.split(' ')
+    for line in lines:
+        if 'www' in line:
+            if 'http' in line:
+                Vdesc = Vdesc.replace(line, f"<a class='link' href='{line}'>{line}</a>")
+            else:
+                temp = 'https://' + line
+                Vdesc = Vdesc.replace(line, f"<a class='link' href='{temp}'>{line}</a>")
+        elif 'http' in line:
+            Vdesc = Vdesc.replace(line, f"<a class='link' href='{line}'>{line}</a>")
     try:
-        views = format(int(data["items"][0]["statistics"]["viewCount"]), ",d")
+        Vviews = format(int(data["items"][0]["statistics"]["viewCount"]), ",d")
     except KeyError:
-        views = 'hidden'
-    try: 
-        likes = format(int(data["items"][0]["statistics"]["likeCount"]), ",d")
-        dislikes = format(int(data["items"][0]["statistics"]["dislikeCount"]), ",d")
+        Vviews = 'hidden'
+    try:
+        Vlikes = format(int(data["items"][0]["statistics"]["likeCount"]), ",d")
+        Vdislikes = format(int(data["items"][0]["statistics"]["dislikeCount"]), ",d")
     except KeyError:
-        likes = 'hidden'
-        dislikes = 'hidden'
+        Vlikes = 'hidden'
+        Vdislikes = 'hidden'
     GETp2()
 
 
@@ -379,25 +343,8 @@ def GETp2():
 88. ~8~ 88.        88         88           j88.
  Y888P  Y88888P    YP         88           888888D
     '''
-    url = f"https://www.googleapis.com/youtube/v3/search" \
-          f"?part=snippet" \
-          f"&maxResults=10" \
-          f"&relatedToVideoId={vId}" \
-          f"&relevanceLanguage=en" \
-          f"&type=video" \
-          f"&videoEmbeddable=true" \
-          f"&fields=items" \
-          "(id" \
-          "/videoId" \
-          ",snippet" \
-          "(channelTitle" \
-          ",thumbnails" \
-          "/medium" \
-          "/url" \
-          ",title))" \
-          f"&key={key}"
+    url = link(Url='p2', Id=vId)
     req = ajax.ajax()
-    print(url)
     req.bind('complete', DONEp2)
     req.open('GET', url, True)
     req.set_header('content-type', 'application/x-www-form-urlencoded')
@@ -420,7 +367,8 @@ Y8888D'  `Y88P'  VP   V8P Y88888P      88           888888D
         vTITLE = video["snippet"]["title"]
         vIMG = video["snippet"]["thumbnails"]["medium"]["url"]
         cTITLE = video["snippet"]["channelTitle"]
-        related.append([vID, vTITLE, vIMG, cTITLE])
+        cID = video["snippet"]["channelId"]
+        related.append([vID, vTITLE, vIMG, cTITLE, cID])
     SHOWp(related)
 
 
@@ -439,26 +387,27 @@ db   8D 88   88 `8b  d8' `8b d8'8b d8'      88
         vTITLE = video[1]
         vIMG = video[2]
         cTITLE = video[3]
-        cooking.append(f"<li><a href='?id={vID}'><img src='{vIMG}'><a/><div>"
-                       f"<a class='vtitle' href='?id={vID}'>{vTITLE}</a>"
-                       f"<p class='ctitle'>{cTITLE}</p></div></li>")
+        cID = video[4]
+        cooking.append(f"<li><a href='?vid={vID}'><img src='{vIMG}' height='120px' width='210px'><a/><div>"
+                       f"<p class='vtitle'><a href='?vid={vID}' class='vtitle'>{vTITLE}</a></p>"
+                       f"<p class='ctitle'><a href='?cid={cID}' class='ctitle'>{cTITLE}</a></p></div></li>")
     cooked = f"<ul class='other'>{''.join(cooking)}</ul>"
     doc["list"].html = f"<div class='grid-video-container'>" \
                        f"<div class='grid-embed'>" \
-                       f"   <iframe src='{link}' " \
+                       f"   <iframe src='{Vembed}' " \
                        f"frameborder='0' allowfullscreen class='player'>" \
                        f"   </iframe></div>" \
                        f"<div class='grid-info'>" \
-                       f"<p class='title'>{title}</p>" \
-                       f"<p class='views'>{views} Views   &#128077; {likes}   &#128078; {dislikes}</p>" \
-                       f"<p class='channel'>{channel}</p>" \
-                       f"<p class='desc'>{desc}</p>" \
+                       f"<p class='title'>{Vtitle}</p>" \
+                       f"<p class='views'>{Vviews} Views   &#128077; {Vlikes}   &#128078; {Vdislikes}</p>" \
+                       f"<a class='channel' href='?cid={VchannelId}'>{Vchannel}</a>" \
+                       f"<p class='desc'>{Vdesc}</p>" \
                        f"</div>" \
                        f"<div class='grid-other'>{cooked}</div>" \
                        f"</div>"
     loaded(True)
-'''
-'''
+
+
 '''
 '''
 '''
@@ -487,8 +436,8 @@ Y8,            88        88    d8""""""""8b    88    `8b 88  88    `8b 88  88   
 '''
 '''
 '''
-'''
-'''
+
+
 def GETc1():
     '''
  d888b  d88888b d888888b       .o88b.       db
@@ -498,17 +447,7 @@ def GETc1():
 88. ~8~ 88.        88         Y8b  d8       88
  Y888P  Y88888P    YP          `Y88P'       VP
     '''
-    url = f"https://www.googleapis.com/youtube/v3/search" \
-          f"?part=snippet" \
-          f"&maxResults=20" \
-          f"&order={order}" \
-          f"&q={q}" \
-          f"&relevanceLanguage=en" \
-          f"&type=channel" \
-          f"&pageToken={page}" \
-          f"&fields=items/id/channelId" \
-          f",nextPageToken,prevPageToken" \
-          f"&key={key}"
+    url = link(Url='c1', Order=order, Q=q, Page=page)
     req = ajax.ajax()
     req.bind('complete', DONEc1)
     req.open('GET', url, True)
@@ -545,8 +484,7 @@ Y8888D'  `Y88P'  VP   V8P Y88888P       `Y88P'       VP
         GETc2(channelIDs)
 
 
-
-def GETc2(raw):
+def GETc2(Ids):
     '''
  d888b  d88888b d888888b       .o88b.      .d888b.
 88' Y8b 88'     `~~88~~'      d8P  Y8      VP  `8D
@@ -555,22 +493,7 @@ def GETc2(raw):
 88. ~8~ 88.        88         Y8b  d8      j88.
  Y888P  Y88888P    YP          `Y88P'      888888D
     '''
-    url = f"https://www.googleapis.com/youtube/v3/channels" \
-          f"?part=snippet,statistics" \
-          f"&id={raw}" \
-          f"&fields=items" \
-          f"(id" \
-          f",snippet" \
-          f"(thumbnails" \
-          f"/medium" \
-          f"/url" \
-          f",title)" \
-          f",statistics" \
-          f"(subscriberCount" \
-          f",viewCount))" \
-          f",nextPageToken,prevPageToken" \
-          f"&key={key}"
-    print(url)
+    url = link(Url='c2', Id=Ids)
     req = ajax.ajax()
     req.bind('complete', DONEc2)
     req.open('GET', url, True)
@@ -598,7 +521,7 @@ Y8888D'  `Y88P'  VP   V8P Y88888P       `Y88P'      888888D
             cVIEWS = format(int(video['statistics']['viewCount']), ',d')
             print(f"{cID} -- CHANNEL -- {cSUBS} -- {cTITLE}")
             cRAWs.append(f"<li class='video'><a href='?cid={cID}'>"
-                         f"<div class='img'><img src='{cIMG}' height='160px' width='210px'></div>"
+                         f"<div class='img channel'><img src='{cIMG}' height='200px' width='200px'></div>"
                          f"<p class='title'>{cTITLE}</p></a>"
                          f"<p class='channel'>"
                          f"{cSUBS} Subscriber<br>{cVIEWS} Views</p>"
@@ -607,17 +530,14 @@ Y8888D'  `Y88P'  VP   V8P Y88888P       `Y88P'      888888D
         SHOWc(cSTRs)
 
 
-
-
-
-def SHOWc(vSTRs):
+def SHOWc(cSTRs):
     '''
-.d8888. db   db  .d88b.  db   d8b   db      db    db
-88'  YP 88   88 .8P  Y8. 88   I8I   88      88    88
-`8bo.   88ooo88 88    88 88   I8I   88      Y8    8P
-  `Y8b. 88~~~88 88    88 Y8   I8I   88      `8b  d8'
-db   8D 88   88 `8b  d8' `8b d8'8b d8'       `8bd8'
-`8888Y' YP   YP  `Y88P'   `8b8' `8d8'          YP
+.d8888. db   db  .d88b.  db   d8b   db       .o88b.
+88'  YP 88   88 .8P  Y8. 88   I8I   88      d8P  Y8
+`8bo.   88ooo88 88    88 88   I8I   88      8P
+  `Y8b. 88~~~88 88    88 Y8   I8I   88      8b
+db   8D 88   88 `8b  d8' `8b d8'8b d8'      Y8b  d8
+`8888Y' YP   YP  `Y88P'   `8b8' `8d8'        `Y88P'
     '''
     doc["list"].html = f"<ul style='" \
                        f"height: 100%;" \
@@ -627,7 +547,7 @@ db   8D 88   88 `8b  d8' `8b d8'8b d8'       `8bd8'
                        f"overflow-y: scroll;" \
                        f"list-style-type: none;" \
                        f"'><div class='grid-videos-container'>" \
-                       f"{vSTRs}" \
+                       f"{cSTRs}" \
                        f"<li></li><li></li><li>" \
                        f"<form style='display: inline;'>" \
                        f"<input type='hidden' name='vorc' value='{vorc}'>" \
@@ -644,6 +564,116 @@ db   8D 88   88 `8b  d8' `8b d8'8b d8'       `8bd8'
                        f"<input type='hidden' name='pageNum' value='{nextPAGEnum}'>" \
                        f"<button type='submit' name='page' value='{nextPAGE}'>&#8250;</button>" \
                        f"</form></li>"
+    loaded(True)
+
+
+'''
+'''
+'''
+'''
+'''
+ ad88888ba   88888888888         db         88888888ba     ,ad8888ba,   88        88
+d8"     "8b  88                 d88b        88      "8b   d8"'    `"8b  88        88
+Y8,          88                d8'`8b       88      ,8P  d8'            88        88
+`Y8aaaaa,    88aaaaa          d8'  `8b      88aaaaaa8P'  88             88aaaaaaaa88
+  `"""""8b,  88"""""         d8YaaaaY8b     88""""88'    88             88""""""""88
+        `8b  88             d8""""""""8b    88    `8b    Y8,            88        88
+Y8a     a8P  88            d8'        `8b   88     `8b    Y8a.    .a8P  88        88
+ "Y88888P"   88888888888  d8'          `8b  88      `8b    `"Y8888Y"'   88        88
+
+
+88        88   ad88888ba   88888888888  88888888ba
+88        88  d8"     "8b  88           88      "8b
+88        88  Y8,          88           88      ,8P
+88        88  `Y8aaaaa,    88aaaaa      88aaaaaa8P'
+88        88    `"""""8b,  88"""""      88""""88'
+88        88          `8b  88           88    `8b
+Y8a.    .a8P  Y8a     a8P  88           88     `8b
+ `"Y8888Y"'    "Y88888P"   88888888888  88      `8b
+'''
+'''
+'''
+'''
+'''
+
+
+def GETu1():
+    '''
+ d888b  d88888b d888888b      db    db       db
+88' Y8b 88'     `~~88~~'      88    88      o88
+88      88ooooo    88         88    88       88
+88  ooo 88~~~~~    88         88    88       88
+88. ~8~ 88.        88         88b  d88       88
+ Y888P  Y88888P    YP         ~Y8888P'       VP
+    '''
+    url = link(Url='u1', Id=cId)
+    req = ajax.ajax()
+    req.bind('complete', DONEu1)
+    req.open('GET', url, True)
+    req.set_header('content-type', 'application/x-www-form-urlencoded')
+    req.send()
+
+
+def DONEu1(req):
+    '''
+d8888b.  .d88b.  d8b   db d88888b      db    db       db
+88  `8D .8P  Y8. 888o  88 88'          88    88      o88
+88   88 88    88 88V8o 88 88ooooo      88    88       88
+88   88 88    88 88 V8o88 88~~~~~      88    88       88
+88  .8D `8b  d8' 88  V888 88.          88b  d88       88
+Y8888D'  `Y88P'  VP   V8P Y88888P      ~Y8888P'       VP
+    '''
+    data = loads(req.text)
+    print(data)
+    data = data["items"][0]
+    title = data["snippet"]["title"]
+    description = data["snippet"]["description"]
+    img = data["snippet"]["thumbnails"]["medium"]["url"]
+    banner = data["brandingSettings"]["image"]["bannerImageUrl"]
+    uploads = data["contentDetails"]["relatedPlaylists"]["uploads"]
+    try:
+        views = format(int(data["statistics"]["viewCount"]), ",d")
+    except KeyError:
+        views = 'hidden'
+    try:
+        subs = format(int(data["statistics"]["subscriberCount"]), ",d")
+    except KeyError:
+        subs = 'hidden'
+    try:
+        videos = format(int(data["statistics"]["videoCount"]), ",d")
+    except KeyError:
+        videos = 'hidden'
+    cooked = [title, description, img, banner, uploads, views, subs, videos]
+    SHOWu(cooked)
+
+
+def SHOWu(raw):
+    '''
+.d8888. db   db  .d88b.  db   d8b   db      db    db
+88'  YP 88   88 .8P  Y8. 88   I8I   88      88    88
+`8bo.   88ooo88 88    88 88   I8I   88      88    88
+  `Y8b. 88~~~88 88    88 Y8   I8I   88      88    88
+db   8D 88   88 `8b  d8' `8b d8'8b d8'      88b  d88
+`8888Y' YP   YP  `Y88P'   `8b8' `8d8'       ~Y8888P'
+    '''
+    title = raw[0]
+    description = raw[1]
+    img = raw[2]
+    banner = raw[3]
+    uploads = raw[4]
+    views = raw[5]
+    subs = raw[6]
+    videos = raw[7]
+    doc["list"].html = str(
+        f"{title}<br>"
+        f"{description}<br>"
+        f"{img}<br>"
+        f"{banner}<br>"
+        f"{uploads}<br>"
+        f"{views}<br>"
+        f"{subs}<br>"
+        f"{videos}<br>"
+    )
     loaded(True)
 
 
